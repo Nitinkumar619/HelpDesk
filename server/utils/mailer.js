104ac68805dda9e5176404db44d8dfd168053690
Nitin Kumar <nitinkumarr619@gmail.com>
2026-07-06 21:50:22 +0530

    Try explicit SMTP port 587 for Gmail to fix connection timeout

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // STARTTLS, not implicit TLS
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
+  connectionTimeout: 10000, // fail fast (10s) instead of hanging
 });
 
 
